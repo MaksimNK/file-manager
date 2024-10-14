dc2641528de5b1c9d7f9db3ff4194b07b163f7ab
MaksimNK <maksmozir@gmail.com>
2024-10-14 23:51:38 +0300

    feat: add utility scripts for input handling and path management

---
+++ src/utils/cli.js
@@ -0,0 +1,47 @@
+import readline from 'readline';
+import {handleOsCommand} from '../os/osCommandHandler.js';
+import { handleFsCommand } from '../fs/fsCommandHandler.js';
+import {COMMANDS_FS, COMMANDS_OS, COMMANDS_NAVIGATE, COMMANDS_HASH, COMMANDS_COMPRESS} from '../constants/commands.js';
+import { getCurrentDirectory, getPath } from './pathService.js';
+import { handleHashCommand } from '../hash/hashCommandHandler.js';
+import { handleNavigationCommand } from '../navigation/navigationCommandHandler.js';
+import { handleCompressCommand } from '../compress/compressCommandHandler.js';
+
+export const readCLI = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.on('line', async (input) => {        
+        const [command, ...args] = input.trim().split(' ');
+        const upperCommand = command.toUpperCase();
+        
+
+        switch (true) {
+            case COMMANDS_FS.hasOwnProperty(upperCommand):
+                await handleFsCommand(command, args);
+                break;
+            case COMMANDS_OS.hasOwnProperty(upperCommand):
+                await handleOsCommand(args[0]);
+                break;
+            case COMMANDS_NAVIGATE.hasOwnProperty(upperCommand):
+                await handleNavigationCommand(command, args);
+                break;
+            case COMMANDS_HASH.hasOwnProperty(upperCommand):
+                await handleHashCommand(command, args);
+                break;
+            case COMMANDS_COMPRESS.hasOwnProperty(upperCommand):
+                await handleCompressCommand(command, args);
+                break;
+            default:
+                console.log('lox');
+                break;
+            }
+        console.log(`You are currently in ${getCurrentDirectory()}`);
+    });
+
+
+          
+//process.exit(0);
+    
+} 
